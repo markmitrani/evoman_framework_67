8f28fdd9258e3b7b021ea49178616c4723ff3fe2
Martin Carrasco <martin.carrasco@utec.edu.pe>
2023-09-22 19:36:50 +0200

    FIX: Implementation of test for ES with cellular niching

--- test_deap.py
+++ test_deap.py
@@ -1,7 +1,9 @@
 import random
 import os
 import numpy as np
+import array
 from typing import Dict, List
+from itertools import chain
 
 from deap import base, creator, tools, algorithms
 
@@ -9,6 +11,7 @@ from evoman.environment import Environment
 from deap_constants import CXPB, MUTPB, calculate_ind_size
 from deap_algorithms import *
 from nn_controller import player_controller
+from graph import GridGraph, manhattan_distance
 
 
 def evaluate(env, individual):
@@ -16,11 +19,14 @@ def evaluate(env, individual):
     #env.player_controller.set(individual, 20)
     f,p,e,t = env.play(pcont=individual)
     # Added the +10 because selection algorithms don't work on negative numbers / 0
-    return (f+10, )
+    return (f, )
 
 
 NGEN = 50
-H_NODES_LAYERS = [20, 10]
+POPULATION_SIZE = 15
+GRID_N_SIZE = 5
+GRID_M_SIZE = 5
+H_NODES_LAYERS = [1]
 IND_SIZE = calculate_ind_size(H_NODES_LAYERS)
 MIN_VALUE = -30
 MAX_VALUE = 30
@@ -50,9 +56,9 @@ def self_adaptive_correlated_mutation(ind):
         ind.strategy[i] *= np.exp(random.gauss(0, 1))
 
     for i in range(len(ind.strategy)):
-        ind.strategy[i] += random.gauss(0, ind.strategy[i])
+        ind[i] += random.gauss(0, ind.strategy[i])
 
-    return ind,
+    return ind
 
 
 def generateES(icls, scls, size, imin, imax, smin, smax):
@@ -75,25 +81,21 @@ def checkStrategy(minstrategy):
 # DEAP Configurations
 
 creator.create('FitnessMax', base.Fitness, weights=(1.0,))
-creator.create('Individual', np.ndarray, fitness=creator.FitnessMax, strategy=None)
-creator.create('Strategy', np.ndarray, typecode="d")
+creator.create('Individual', array.array, fitness=creator.FitnessMax, strategy=None, typecode='d')
+creator.create('Strategy', array.array, typecode='d')
 
 toolbox = base.Toolbox()
 toolbox.register('attr_float', random.random)
-#toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.attr_float, n=IND_SIZE)
 toolbox.register("individual", generateES, creator.Individual, creator.Strategy,
     IND_SIZE, MIN_VALUE, MAX_VALUE, MIN_STRATEGY, MAX_STRATEGY)
 toolbox.register('population', tools.initRepeat, list, toolbox.individual)
-toolbox.register('evaluate', evaluate, env)
-#toolbox.register("select", tools.selTournament, tournsize=3)
-#toolbox.register("mate", tools.cxESBlend, alpha=0.1)
-toolbox.register("mate", tools.cxUniform, indpb=0.2)
-toolbox.register("mutate", self_adaptive_correlated_mutation)
 
-#toolbox.decorate("mate", checkStrategy(MIN_STRATEGY))
-toolbox.decorate("mutate", checkStrategy(MIN_STRATEGY))
-#toolbox.register("mutate", self_adaptive_correlated_mutation, sigma=[initial_sigma] * IND_SIZE)
-toolbox.register("select", tools.selTournament, tournsize=4)
+toolbox.register('evaluate', evaluate, env)
+toolbox.register("mate", tools.cxESTwoPoint)
+toolbox.register("mutate", self_adaptive_correlated_mutation)
+toolbox.register("select", tools.selTournament, tournsize=2)
+
+#toolbox.decorate("mutate", checkStrategy(MIN_STRATEGY))
 
 stats = tools.Statistics(key=lambda ind: ind.fitness.values)
 stats.register("avg", np.mean)
@@ -105,37 +107,87 @@ hof = tools.HallOfFame(maxsize=1)
 winner = None
 
 def main():
-    pop = toolbox.population(n=150)
-    # Constants for Mutation / Crossover
-    fitnesses = map(toolbox.evaluate, pop)
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
+    # Initialize grid graph with populations
+    gg = GridGraph(GRID_N_SIZE, GRID_M_SIZE, pop_size=POPULATION_SIZE, toolbox=toolbox)
 
+
+
+    gg.update_fitnesses(toolbox)
     for g in range(NGEN):
-        # Crossover and Mutation
-        offspring = algorithms.varAnd(pop, toolbox, CXPB, MUTPB)
+        # Iterate over each deme
+        new_gg: GridGraph = gg.deepcopy()
+        for node in gg: 
+            
+            node: GridGraph.Node = node
+            clone_node: GridGraph.Node = new_gg[node.get_coords()[0]][node.get_coords()[1]]
+            node_pop = node.get_pop()
 
-        # Evaluate fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit
+            # Get the population of all the neighbouring nodes
+            neighbour_pop = [n_i.get_pop() for n_i in gg.get_knn(node.get_coords()[0], node.get_coords()[1], manhattan_distance, max_dist=1)]
+            neighbour_pop = list(chain.from_iterable(neighbour_pop))
 
-        # Replacement
-        #pop[:] = offspring
-        pop = toolbox.select(pop + offspring, len(pop))
+            for ind_idx, ind in enumerate(node_pop):
+                offspring = toolbox.clone(ind)
+                change_ocurred = False
 
-        #TODO How to ?
-        # TODO Update mutation parameters here
+                # Crossover
+                if random.random() < CXPB:
+                    change_ocurred = True
+                    partner = random.choice(neighbour_pop)
+                    while offspring == partner:
+                        partner = random.choice(neighbour_pop)
 
+                    offspring_1, offspring_2 = toolbox.mate(offspring, partner)
+
+                    offspring = random.choice([offspring_1, offspring_2])
+
+                    del offspring.fitness.values
+
+                # Mutation
+                if random.random() < MUTPB:
+                    change_ocurred = True
+                    offspring = toolbox.mutate(offspring)
+                    del offspring.fitness.values
+
+                if not change_ocurred:
+                    continue
+                
+                offspring.fitness.values = toolbox.evaluate(offspring)
+
+                # Replacement
+                if offspring.fitness.values[0] >= ind.fitness.values[0]:
+                    clone_node[ind_idx] = offspring
+                else:
+                    assert(offspring != ind)
+                    clone_node[ind_idx] = ind
+            
+
+            #record = stats.compile(clone_node.get_popu())
+            #print(f'gen: {g}, node: {node}, record: {record}')
+
+
+
+        gg = new_gg
+        allpop = gg.allpop()
+        record = stats.compile(allpop)
+        candidate_winner = allpop[np.argmax([i.fitness.values[0] for i in allpop])]
+        global winner
+        if type(winner) is not creator.Individual:
+            winner = candidate_winner
+        winner = winner if winner.fitness > candidate_winner.fitness else candidate_winner
+        print(candidate_winner.fitness)
+        print(f'gen: {g}, record: {record}')
+
+        '''
         #env.update_solutions(pop)
-        record = stats.compile(pop)
+
         candidate_winner = pop[np.argmax([i.fitness for i in pop])]
         global winner
         if type(winner) is not creator.Individual:
             winner = candidate_winner
         winner = winner if winner.fitness > candidate_winner.fitness else candidate_winner
         print(g, record)
+        '''
 
 main()
 env.update_parameter("speed", "normal")
